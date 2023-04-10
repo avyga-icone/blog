f20035170bb0b0f6566e02242a45b8d3f0a663e4
jbob9 <iconedevultra@gmail.com>
2023-04-10 15:32:34 -0400

    remove svelte add aplinejs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,23 +1,25 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import i18n from "astro-i18n";
 import sitemap from '@astrojs/sitemap';
-import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 import sanity from "astro-sanity";
-import i18n from "astro-i18n"
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [
     mdx(), 
-    i18n(),
-    sitemap(), svelte(), tailwind(),
+    i18n(), 
+    sitemap(), 
+    tailwind(), 
+    alpinejs(),
     sanity({
       projectId: '7bqqhdtu',
       dataset: 'developement',
       apiVersion: '2023-03-01',
-      useCdn: false,
-    })
+      useCdn: false
+    }), 
   ]
 });
