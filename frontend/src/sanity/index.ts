7984c0a17d24979ef05b1c0c15885ca6a8a3c765
jbob9 <iconedevultra@gmail.com>
2023-04-11 14:11:36 -0400

    add support for dark mode

--- frontend/src/sanity/index.ts
+++ frontend/src/sanity/index.ts
@@ -1,3 +1,53 @@
 export * from "./portableText";
 export * from "./queries";
 export * from "./urlForImage"
+
+export type Image = {
+  _type: 'image',
+  asset: {
+    _ref: string,
+    _type: 'reference'
+  }
+};
+
+export type Post = {
+  author: {
+    image: Image,
+    name: string,
+    slug: string,
+    bio: string,
+  },
+  body: {
+    _type: 'localeBlockContent',
+    fr: Array<any>
+  },
+  bio: string;
+  categories: { slug: string, title:  string}[],
+  intro: string,
+  mainImage: Image,
+  publishedAt: string,
+  slug: string,
+  tags: { slug: string, title: string }[],
+  title: string
+}
+
+export type PostAndRecommend = {
+  author: {
+    image: Image,
+    name: string,
+    slug: string,
+    bio: string,
+  },
+  body: {
+    _type: 'localeBlockContent',
+    fr: Array<any>
+  },
+  categories: { slug: string, title:  string}[],
+  intro: string,
+  mainImage: Image,
+  publishedAt: string,
+  slug: string,
+  tags: { slug: string, title: string }[],
+  title: string
+  recommended: Post[]
+}
--- frontend/src/sanity/queries.ts
+++ frontend/src/sanity/queries.ts
@@ -5,17 +5,14 @@ export const getAllPostForHome = groq`*[_type == "post"]{
 	"slug": slug.current,
 	mainImage,
 	publishedAt,
-	"category": category->{
+	"categories": categories[]->{
 		title,
 		"slug": slug.current
 	},
-}`;
+}[0...9]`;
 
-export const getAllPostSlug = groq`*[_type == 'post']{
-  "slug": slug.current
-}`;
 
-export const getAllPostForPage = groq`*[_type == "post"]{
+export const getAllPostAndRecommended = groq`*[_type == "post"]{
 	title,
 	"slug": slug.current,
 	mainImage,
@@ -24,9 +21,11 @@ export const getAllPostForPage = groq`*[_type == "post"]{
 	"author": author->{
 		name,
 		image,
+		slug,
+		bio
 	},
 	body,
-	"category": category->{
+	"categories": categories[]->{
 		title,
 		"slug": slug.current,
 	},
@@ -34,21 +33,18 @@ export const getAllPostForPage = groq`*[_type == "post"]{
 		title,
 		"slug": slug.current,
 	},
+	"recommended": *[
+		_type == "post" 
+		&& categories[0]->{'slug': slug.current}.slug == ^.categories[0]->{ 'slug': slug.current }.slug
+		&& slug.current != ^.slug.current
+	]{
+		title,
+		"slug": slug.current,
+		mainImage,
+		publishedAt,
+		"categories": categories[]->{
+			title,
+			"slug": slug.current
+		}
+	}[0...2]
 }`
-
-// // title,
-// 	"slug": slug.current,
-// 	mainImage,
-// 	publishedAt,
-// 	"author": author->{
-// 		name,
-// 		image,
-// 	},
-// 	categories{
-// 		title,
-// 		"slug": slug.current,
-// 	},
-// 	tags{
-// 		title,
-// 		"slug": slug.current,
-// 	},
