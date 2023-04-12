a1560da8809121d64b0ad517a63e3f1f22f88a23
jbob9 <iconedevultra@gmail.com>
2023-04-11 20:46:32 -0400

    remove unused packages

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,20 +1,16 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import i18n from "astro-i18n";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
 import sanity from "astro-sanity";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [
-    mdx(), 
     i18n(), 
     sitemap(), 
     tailwind(), 
-    alpinejs(),
     sanity({
       projectId: '7bqqhdtu',
       dataset: 'developement',
