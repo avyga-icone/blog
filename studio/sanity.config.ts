32fd41f7617cea862a354f56f5478aae8ebfde0f
jbob9 <iconedevultra@gmail.com>
2023-04-11 20:47:28 -0400

    add dashboard plugio in my studio

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,6 +1,11 @@
 import {defineConfig, isDev } from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
+import {
+  dashboardTool,
+  projectUsersWidget,
+  projectInfoWidget,
+} from "@sanity/dashboard";
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
@@ -8,9 +13,18 @@ export default defineConfig({
   title: 'avyga-blog',
 
   projectId: '7bqqhdtu',
-  dataset: 'developement',
+  dataset: isDev ? 'developement' : 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool(), 
+    visionTool(),
+    dashboardTool({
+      widgets: [
+        projectUsersWidget(),
+        projectInfoWidget()
+      ]
+    })
+  ],
 
   schema: {
     types: schemaTypes,
