6595c23dfac75dcb95355b61a02c745e66fa768c
jbob9 <berlenskyoliver99@gmail.com>
2023-04-14 14:20:08 -0400

    list the first 5 categories on the home page and arrange rss function

--- frontend/src/pages/rss.xml.js
+++ frontend/src/pages/rss.xml.js
@@ -1,16 +1,20 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { useSanityClient } from 'astro-sanity';
+import { getAllPostForRss  } from '../sanity';
 
 export async function get(context) {
-	const posts = await getCollection('blog');
+  const posts = await useSanityClient().fetch(getAllPostForRss);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
+		items: posts.map((post) => {
+			const { slug, ...rest } = post;
+			return {
+				...rest,
+				link: `/blog/${post.slug}/`,
+			}
+		}),
 	});
 }
--- frontend/src/sanity/queries.ts
+++ frontend/src/sanity/queries.ts
@@ -1,5 +1,10 @@
 import { groq } from 'astro-sanity';
 
+export const getCateogriesPaginated = groq`*[_type == "category"]{
+	title,
+	"slug": slug.current
+}[0...5]`
+
 export const getAllPostForHome = groq`*[_type == "post" && dateTime(publishedAt) <= dateTime(now())] | order(publishedAt desc){
 	title,
 	"slug": slug.current,
@@ -9,6 +14,17 @@ export const getAllPostForHome = groq`*[_type == "post" && dateTime(publishedAt)
 		title,
 		"slug": slug.current
 	},
+}`;
+
+export const getAllPostForRss = groq`*[
+	_type == "post" 
+	&& dateTime(publishedAt) <= dateTime(now())
+	&& title.fr != null
+] | order(publishedAt desc){
+	"title": title.fr,
+	"pubDate": publishedAt,
+	"description": intro.fr,
+	"slug": slug.current
 }`;
 
 export const getAllPostForHomePaginated = groq`*[_type == "post" && dateTime(publishedAt) <= dateTime(now())] | order(publishedAt desc) {
